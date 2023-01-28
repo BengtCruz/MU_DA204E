e6e6e5ae8d540ef0d3e7247a2bad888ba8a4e90b
BengtCruz <bengtsvedling@hotmail.com>
2023-01-28 20:12:32 +0100

    Start A2

---
+++ A2/A2/FunFeatures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public class FunFeatures
+    {
+        private string name;
+        private string email;
+
+        private void introduce()
+        {
+
+        }
+
+    }
+}
---
+++ A2/A2/MainProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace A2
+{
+    internal class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
