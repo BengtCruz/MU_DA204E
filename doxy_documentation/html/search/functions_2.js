44cf4e03e472d154548f6e759488f4b19cd1d0a8
BengtCruz <bengtsvedling@hotmail.com>
2023-02-04 19:08:18 +0100

    Assignment 2D

--- doxy_documentation/html/search/functions_2.js
+++ doxy_documentation/html/search/functions_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['showmenu_0',['ShowMenu',['../class_a2_1_1_temperature_converter.html#a8a2a9992ffb95a27ecde162c9e6795b1',1,'A2::TemperatureConverter']]],
-  ['start_1',['Start',['../class_a2_1_1_temperature_converter.html#a34dace1d786358e375119065aa08bfa7',1,'A2::TemperatureConverter']]]
+  ['requestandshowlist_0',['RequestAndShowList',['../class_a2_1_1_working_schedule.html#a2e355c006bc79888be4643dd2d366192',1,'A2::WorkingSchedule']]]
 ];
